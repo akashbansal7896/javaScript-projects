af0c3447400a250052e891f390bb6cba8f966234
Akash bansal <bansalakash7896@gmail.com>
2024-06-04 10:03:06 +0530

    1.0

---
+++ Analog clock/script.js
@@ -0,0 +1,17 @@
+setInterval(()=>{
+    let d = new Date();
+    let htime = d.getHours();
+    let mtime = d.getMinutes();
+    let stime = d.getSeconds();
+
+
+    let hrotation = 30*htime + mtime/2 + stime/120;
+    let mrotation= 6*mtime + stime/10;
+    let srotation= 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+
+    
+},1000);
